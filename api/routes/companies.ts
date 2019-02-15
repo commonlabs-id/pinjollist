49d4323c8e889fd3187dff285d1906d634323859
Resi Respati <resir014@gmail.com>
2019-02-15 23:45:39 +0700

    fixed error handling

--- api/routes/companies.ts
+++ api/routes/companies.ts
@@ -3,25 +3,35 @@ import { app } from '../utils/firebase';
 
 export default async function handler(_: IncomingMessage, res: ServerResponse) {
   if (app.firestore) {
-    const { docs } = await app
-      .firestore()
-      .collection('lending.services')
-      .get();
+    try {
+      const { docs } = await app
+        .firestore()
+        .collection('lending.services')
+        .get();
 
-    res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(
+        JSON.stringify({
+          status: 'ok',
+          data: docs,
+        }),
+      );
+    } catch (err) {
+      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.end(
+        JSON.stringify({
+          status: 'error',
+          message: err.message,
+        }),
+      );
+    }
+  } else {
+    res.writeHead(400, { 'Content-Type': 'application/json' });
     res.end(
       JSON.stringify({
-        status: 'ok',
-        data: docs,
+        status: 'error',
+        message: 'Failed to connect to Firebase.',
       }),
     );
   }
-
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(
-    JSON.stringify({
-      status: 'error',
-      message: 'Failed to connect to Firebase.',
-    }),
-  );
 }
