f29fa3202e342f90cfac9be7b4a2510fa27585c1
Resi Respati <resir014@gmail.com>
2019-02-16 13:53:00 +0700

    feat(api): firebase integration (#2)
    * feat: firebase integration
    
    * fixed error handling
    
    * fixed typo
    
    * ok.
    
    * refactor response builder code
    
    * pretty routes (need a better way to do this)

---
+++ api/utils/firebase.ts
@@ -0,0 +1,13 @@
+import firebase from '@firebase/app';
+import '@firebase/firestore';
+
+export const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_SENDER_ID,
+};
+
+export const app = firebase.initializeApp(config);
