df7ce52f6ad4295bd06b81ffb8a9e408b5ff26eb
Resi Respati <resir014@gmail.com>
2019-02-19 02:59:06 +0700

    added (blurry) opengraph image

--- www/components/SEO.tsx
+++ www/components/SEO.tsx
@@ -9,6 +9,8 @@ const SEO: React.FC = () => (
     <meta name="og:url" content="https://apakah-terdaftar-di-ojk.netlify.com" />
     <meta name="description" content="Database jasa P2P Lending yang berizin OJK." />
     <meta name="og:description" content="Database jasa P2P Lending yang berizin OJK." />
+    <meta name="og:image" content="https://pinjollist.now.sh/static/images/pinjollist-og.png" />
+    <meta name="twitter:card" content="summary_large_image" />
     <link rel="icon" type="image/png" sizes="32x32" href="/static/images/favicon-32x32.png" />
     <link rel="icon" type="image/png" sizes="16x16" href="/static/images/favicon-16x16.png" />
     <link rel="shortcut icon" href="/static/images/favicon.ico" />
