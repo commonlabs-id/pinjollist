2148193719ff9777483f117a63b0859f23293f63
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2019-05-04 11:05:02 +0700

    fix: remove unneeded process env

--- api/utils/firestore.ts
+++ api/utils/firestore.ts
@@ -1,16 +1,9 @@
 import admin from 'firebase-admin';
 
 const serviceAccount: admin.ServiceAccount = {
-  // type: 'service_account',
   projectId: process.env.FIREBASE_PROJECT_ID,
-  // private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
   privateKey: Buffer.from(process.env.FIREBASE_PRIVATE_KEY_BASE64 || '', 'base64').toString(),
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  // client_id: process.env.FIREBASE_CLIENT_ID,
-  // auth_uri: process.env.FIREBASE_AUTH_URI,
-  // token_uri: process.env.FIREBASE_TOKEN_URI,
-  // auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-  // client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
 };
 
 const config = {
