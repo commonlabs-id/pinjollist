4c0fbbafff59a42c65238c0a9649064945c86b7a
Resi Respati <resir014@gmail.com>
2019-05-14 22:03:29 +0700

    feat: populate realtime stats with non-hardcoded values (#28)

--- www/components/stats/RealtimeStats.tsx
+++ www/components/stats/RealtimeStats.tsx
@@ -16,23 +16,51 @@ interface RealtimeStatsState {
   isLoading: boolean;
   errors?: string;
   data: any[];
+  numSyariah: number;
 }
 
 const initialState: RealtimeStatsState = {
   isLoading: true,
   errors: undefined,
   data: [],
+  numSyariah: 0,
 };
+
+const ErrorText = styled('p')`
+  align-self: center;
+  justify-self: center;
+  width: 100%;
+  margin: 0;
+  color: ${colors.red};
+  text-align: center;
+`;
+
+const Root = styled('div')`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  min-height: 200px;
+  margin-left: -0.75rem;
+  margin-right: -0.75rem;
+
+  @media (min-width: ${breakpoints.md}px) {
+    flex-direction: row;
+    justify-content: center;
+  }
+`;
 
 const RealtimeStats: React.FC<RealtimeStateProps> = ({ data }) => {
   const [state, setState] = React.useState<RealtimeStatsState>(initialState);
 
   const fetchData = async () => {
     if (data) {
+      const numSyariah = data.filter(item => !!item.is_syariah === true).length;
+
       setState({
         ...state,
         isLoading: false,
         data,
+        numSyariah,
       });
     } else {
       try {
@@ -42,10 +70,13 @@ const RealtimeStats: React.FC<RealtimeStateProps> = ({ data }) => {
         const json: APIResponse<any[]> | ErrorAPIResponse = await res.json();
 
         if (json.status === 'ok') {
+          const numSyariah = json.data.filter(item => !!item.is_syariah === true).length;
+
           setState({
             ...state,
             isLoading: false,
             data: json.data,
+            numSyariah,
           });
         } else {
           if (json.status === 'error' && json.data.message) {
@@ -87,33 +118,9 @@ const RealtimeStats: React.FC<RealtimeStateProps> = ({ data }) => {
   return (
     <Root>
       <RealtimeStatsCard number={state.data.length} text="Terdaftar" />
-      <RealtimeStatsCard number={6} text="Dihapus" />
-      <RealtimeStatsCard number={1} text="Terdaftar ulang" />
+      <RealtimeStatsCard number={state.numSyariah} text="Syariah" />
     </Root>
   );
 };
 
 export default RealtimeStats;
-
-const Root = styled('div')`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  min-height: 200px;
-  margin-left: -0.75rem;
-  margin-right: -0.75rem;
-
-  @media (min-width: ${breakpoints.md}px) {
-    flex-direction: row;
-    justify-content: center;
-  }
-`;
-
-const ErrorText = styled('p')`
-  align-self: center;
-  justify-self: center;
-  width: 100%;
-  margin: 0;
-  color: ${colors.red};
-  text-align: center;
-`;
