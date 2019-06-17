ac8ea0f5a8c0920fc25ceb9de2353e52af033e1c
Resi Respati <resir014@gmail.com>
2019-06-17 17:13:18 +0700

    move all route files to root

--- api/ping.ts
+++ api/ping.ts
@@ -1,5 +1,5 @@
 import { send, RequestHandler } from 'micro';
-import buildResponse from '../utils/buildResponse';
+import buildResponse from './utils/buildResponse';
 
 const handler: RequestHandler = async (_, res) => {
   send(res, 200, buildResponse('ok', 'Pong!'));
