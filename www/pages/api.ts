75ec281d380d137c54f61854ef2a7f175231ea54
Resi Respati <resir014@gmail.com>
2019-07-27 02:15:59 +0700

    fix: remove API redirection

--- www/pages/api.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handle(_: NextApiRequest, res: NextApiResponse) {
-  res.writeHead(302, {
-    Location: '/docs',
-  });
-  res.end();
-}
