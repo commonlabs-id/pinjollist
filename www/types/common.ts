619249e15a7890a3333e0dba7a28c8114f71e3e9
Resi Respati <resir014@gmail.com>
2020-01-14 14:13:26 +0700

    refactor: move pinjollist API into Next.js API pages (#207)
    * chore(tsc): add typeRoots to tsconfig
    
    * refactor: migrate to Next.js API Routes
    
    * fix: .d.ts not .ts
    
    * fix: accidentally removed next-env.d.ts
    
    * fix: fix lint + typecheck errors
    
    * fix: periodt
    
    * fix: fixed build env
    
    * chore(now): fix secrets name

--- www/types/common.ts
+++ www/types/common.ts
@@ -1,11 +1,17 @@
-export interface APIResponse<T = {}> {
-  status: 'ok';
+export type ResponseStatuses = 'ok' | 'error';
+
+export interface ResponseObject<T = any> {
+  status: ResponseStatuses;
   data: T;
 }
 
-export interface ErrorAPIResponse {
-  status: 'error';
+export interface ResponseErrorMessage {
   data: {
     message: string;
   };
+}
+
+export interface ServiceAccount {
+  client_email: string;
+  private_key: string;
 }
